bd16acd54464e76dde37f3c1e80b0cc04a3974bd
Charles Chapman <spy_deee@yahoo.com>
2017-05-16 19:47:06 -0700

    new js
    javascript

---
+++ todo.js
@@ -0,0 +1,21 @@
+
+function get_todos() {
+
+	var todos = new Array;
+
+	var todos_str = localStorage.getItem('todo') ;
+
+	if (todos_str != null) {
+
+		todos = JSON.parse(todos_str);
+	}
+
+	return todos;
+}
+
+
+
+
+
+//document.getElementById('add').addEventListener('click', add);//
+//show();//
